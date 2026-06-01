26c2e1fc0a1a31491f08d606883f3fbc30999b93
Iago do Nascimento Marinho <iagoomarinho@gmail.com>
2026-06-01 11:44:22 -0300

    teste

--- src/contextos/LayoutContexto.tsx
+++ src/contextos/LayoutContexto.tsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from 'react'
-import { type ReactNode } from 'react';
+import { type ReactNode } from 'react'
 
 interface LayoutProviderProps {
   children: ReactNode
