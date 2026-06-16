8e4b197182711307127a06f2aa92ab415f937d2b
Iago do Nascimento Marinho <iagoomarinho@gmail.com>
2026-06-16 14:39:41 -0300

    modalmensagem

---
+++ src/componentes/ModalMensagem.tsx
@@ -0,0 +1,30 @@
+import estilos from './ModalMensagem.module.css'
+
+interface ModalMensagemProps {
+    exibir: boolean
+    titulo: string
+    texto: string
+    ocultar: () => void
+}
+
+export function ModalMensagem({exibir, ocultar, titulo, texto}: ModalMensagemProps){
+    if(exibir){
+        return(
+            <div className={estilos.conteiner}>
+                <p className={estilos.titulo}>{titulo}</p>
+
+                <div className={estilos.conteinerMensagem}>
+                    <p className={estilos.mensagem}>{texto}</p>
+                </div>
+
+                <button 
+                    className={estilos.botao}
+                    onClick={ocultar}
+                    >
+                        Fechar
+                </button>
+
+            </div>
+        )
+    }
+}
