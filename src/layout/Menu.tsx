2d40894e5bb5a4d47d5c38c9a3f45983bf1349ba
xarthwr <arthurvitalico@gmail.com>
2026-07-12 21:55:50 -0300

    novo menu part1
    Menu atualizado + divisão do layout

--- src/layout/Menu.tsx
+++ src/layout/Menu.tsx
@@ -1,135 +1,105 @@
 import estilos from './Menu.module.css'
 import { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { LayoutContexto } from '../contextos/LayoutContexto'
 
 import { FaHome } from "react-icons/fa"
 import { FaMagnifyingGlass } from "react-icons/fa6"
-import { FaMapMarked } from "react-icons/fa"
 import { IoIosStats } from "react-icons/io"
 import { FaBook } from "react-icons/fa"
 import { TiThMenu } from "react-icons/ti"
 import { CgProfile } from "react-icons/cg"
 import { FaGlassWaterDroplet } from "react-icons/fa6"
 
-export function Menu() {
+export function Menu(){
 
-    const {
+    const{
         menuAbertoContexto,
         setMenuAbertoContexto
     } = useContext(LayoutContexto)
 
-    const controlarMenu = () =>
-        setMenuAbertoContexto(
-            !menuAbertoContexto
-        )
+    const location = useLocation()
 
-    return (
+    const controlarMenu = () =>{
+        setMenuAbertoContexto(!menuAbertoContexto)
+    }
+
+    return(
+
         <aside
             className={estilos.conteiner}
             style={{
-                width:
-                    menuAbertoContexto
-                        ? '240px'
-                        : '80px'
+                width:menuAbertoContexto ? "240px":"80px"
             }}
         >
+
             <button
                 className={estilos.botaoMenu}
                 onClick={controlarMenu}
             >
-                <TiThMenu />
+                <TiThMenu/>
             </button>
 
             <nav>
 
                 <Link
-                    className={estilos.itemConteiner}
                     to="/home"
+                    className={`${estilos.itemConteiner} ${location.pathname==="/home" ? estilos.ativo:""}`}
                 >
-                    <span><FaHome /></span>
+                    <FaHome/>
 
-                    {menuAbertoContexto && (
-                        <span
-                            className={estilos.rotulo}
-                        >
-                            Home
-                        </span>
-                    )}
+                    {menuAbertoContexto && <span className={estilos.rotulo}>Home</span>}
                 </Link>
 
                 <Link
-                    className={estilos.itemConteiner}
                     to="/detectar"
+                    className={`${estilos.itemConteiner} ${location.pathname==="/detectar" ? estilos.ativo:""}`}
                 >
-                    <span><FaMagnifyingGlass /></span>
+                    <FaMagnifyingGlass/>
 
-                    {menuAbertoContexto && (
-                        <span
-                            className={estilos.rotulo}
-                        >
-                            Detectar
-                        </span>
-                    )}
+                    {menuAbertoContexto && <span className={estilos.rotulo}>Detectar</span>}
                 </Link>
-                 <Link
-                    className={estilos.itemConteiner}
-                    to="/gole"
-                >
-                    <span><FaGlassWaterDroplet /></span>
 
-                    {menuAbertoContexto && (
-                        <span
-                            className={estilos.rotulo}
-                        >
-                            Gole+
-                        </span>
-                    )}
-                </Link>
-                    <Link
-                    className={estilos.itemConteiner}
-                    to="/analises"
-                >
-                    <span><IoIosStats /></span>
-
-                    {menuAbertoContexto && (
-                        <span
-                            className={estilos.rotulo}
-                        >
-                            Analises
-                        </span>
-                    )}
-                </Link>
-                   <Link
-                    className={estilos.itemConteiner}
-                    to="/sobre"
-                >
-                    <span><FaBook /></span>
-
-                    {menuAbertoContexto && (
-                        <span
-                            className={estilos.rotulo}
-                        >
-                            Sobre
-                        </span>
-                    )}
-                </Link>
                 <Link
-                    className={estilos.itemConteiner}
-                    to="/perfil"
+                    to="/gole"
+                    className={`${estilos.itemConteiner} ${location.pathname==="/gole" ? estilos.ativo:""}`}
                 >
-                    <span><CgProfile /></span>
+                    <FaGlassWaterDroplet/>
 
-                    {menuAbertoContexto && (
-                        <span
-                            className={estilos.rotulo}
-                        >
-                            Perfil
-                        </span>
-                    )}
+                    {menuAbertoContexto && <span className={estilos.rotulo}>Gole+</span>}
+                </Link>
+
+                <Link
+                    to="/analises"
+                    className={`${estilos.itemConteiner} ${location.pathname==="/analises" ? estilos.ativo:""}`}
+                >
+                    <IoIosStats/>
+
+                    {menuAbertoContexto && <span className={estilos.rotulo}>Análises</span>}
+                </Link>
+
+                <Link
+                    to="/sobre"
+                    className={`${estilos.itemConteiner} ${location.pathname==="/sobre" ? estilos.ativo:""}`}
+                >
+                    <FaBook/>
+
+                    {menuAbertoContexto && <span className={estilos.rotulo}>Sobre</span>}
+                </Link>
+
+                <Link
+                    to="/perfil"
+                    className={`${estilos.itemConteiner} ${location.pathname==="/perfil" ? estilos.ativo:""}`}
+                >
+                    <CgProfile/>
+
+                    {menuAbertoContexto && <span className={estilos.rotulo}>Perfil</span>}
                 </Link>
 
             </nav>
+
         </aside>
+
     )
+
 }
